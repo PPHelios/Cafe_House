899bd4cbd742ca2d2a592a67bb404660dfaa5e8c
Haytham Atef <103271591+PPHelios@users.noreply.github.com>
2023-04-21 12:04:23 +0200

    Update addProperty.js

--- myHomeB4a/cloud/functions/addProperty.js
+++ myHomeB4a/cloud/functions/addProperty.js
@@ -1,26 +1,82 @@
-Parse.Cloud.define("createProperty", async(req, res)=>{
-    const {adName,adNameAr,description} = req.params;
-    const agency = req.user.get("profile").agency
-    const internalRole = req.user.get("profile").internalRole
-const creator = req.user
-const creatorEmail = req.user.get("email")
-try{
-    if (internalRole === "Agency" || internalRole  === "Moderator" ||internalRole  === "AdCreator"){
-      const newProperty = new Parse.Object("Property")
-      newProperty.set("adName", adName);
-      
-      
-      newProperty.set("creator", creator);
-      newProperty.set("creatorEmail", creatorEmail);
-      
-      const saveProperty = newProperty.save(null, {useMasterKey:true})
-      res.send(saveAgent)
-      
-    }  else {
-      throw new Error("Unauthorized")
-    }
-    } catch(err){
-      throw new Error(err.message)
-    }
-    
-})
+Parse.Cloud.define("addProperty", async (req, res) => { 
+
+const user,userRole,agency,agencyName = null
+if (req.user.get("userRole") === "Agency"){
+     user = await req.user.fetchWithInclude(["agencyPointer"], { 
+     useMasterKey: true, 
+   }); 
+    userRole = user.get("agencyPointer").get("userRole"); 
+    agency = user.get("agencyPointer"); 
+    agencyName = user.get("agentPointer").get("agencyName"); 
+} else if {
+    (["Moderator", "AdCreator"].include(req.user.get("userRole") ){
+     user = await req.user.fetchWithInclude(["agentPointer"], { 
+     useMasterKey: true, 
+   }); 
+    userRole = user.get("agentPointer").get("userRole"); 
+    agency = user.get("agencyPointer"); 
+    agencyName = user.get("agentPointer").get("agencyName");
+} else {
+    throw new Error("Unauthorized"); 
+}
+  
+   let parseFile = null; 
+   const { 
+     firstName, 
+     lastName, 
+     firstNameAr, 
+     lastNameAr, 
+     email, 
+     password, 
+     bio, 
+     bioAr, 
+     phoneNumber, 
+     role, 
+     profilePic, 
+   } = req.params; 
+   // console.log(req.params); 
+   // console.log({ userRole }); 
+   // console.log({ agency }); 
+   // console.log({ role }); 
+   try { 
+            
+  
+       const newProperty = new Parse.Object("Property"); 
+       newProperty.set("adName", adName); 
+       newProperty.set("adNameAr", adNameAr); 
+       newProperty.set("creator", req.user); 
+       newProperty.set("creatorEmail", req.user.get("email")); 
+       newProperty.set("userPointer", createdUser.toPointer()); 
+       
+             
+       if (picFiles) { 
+const picCount = picFiles. length
+for (let i =0 ;i<picCount ;i++){
+    newProperty.set(`pic${i}`, picFiles[i]); 
+}         
+         createdUser.set("picUrls", picUrls); 
+       } 
+       if (videoFiles) { 
+const videoCount = videoFiles. length
+for (let i =0 ;i<videoCount ;i++){
+    newProperty.set(`video${i}`, videoFiles[i]); 
+}         
+         createdUser.set("videoUrls", videoUrls); 
+       }        let propertyACL = new Parse.ACL(); 
+       propertyACL.setPublicReadAccess(true); 
+       propertyACL.setWriteAccess(req.user.id, true); 
+       propertyACL.setRoleWriteAccess("SuperAdmin", true); 
+       propertyACL.setRoleWriteAccess("SubAdmin", true); 
+       propertyACL.setReadAccess(req.user.id, true); 
+       propertyACL.setRoleReadAccess("SuperAdmin", true); 
+       propertyACL.setRoleReadAccess("SubAdmin", true); 
+       createdUser.setACL(propertyACL); 
+       const saveProperty = await newProperty.save(null, { useMasterKey: true }); 
+       console.log({ saveProperty }); 
+       return (saveProperty); 
+     } 
+  
+    
+   } catch (err) { 
+     throw new Error(err.message); 
+   }
