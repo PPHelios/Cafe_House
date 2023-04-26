3d6035be67a91138d5d8a268dec260ac5b48a6a5
PPHelios <pphelios1@gmail.com>
2023-04-26 09:01:43 +0200

    test compress

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import requireTransform from "vite-plugin-require-transform";
-
+//import requireTransform from "vite-plugin-require-transform";
+import viteCompression from 'vite-plugin-compression';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [requireTransform({}), preact()],
+  plugins: [ preact(),viteCompression({algorithm:'brotliCompress'})],
 });
